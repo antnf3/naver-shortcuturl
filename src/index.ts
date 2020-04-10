1d7690b86038bd6981b59200c628845c9fca9f6f
dryadsoft <dryadsoft@gmail.com>
2020-04-10 23:00:23 +0900

    return type  설정

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,50 @@
 import axios from "axios";
 import "dotenv/config";
 
-function getShutCutUrl(beforeUrl: string) {
-    return new Promise((resolve) => {
-        const URL = 'https://openapi.naver.com/v1/util/shorturl.json';
-        const headers = {
-            "X-Naver-Client-Id": process.env.CLIENT_ID,
-            "X-Naver-Client-Secret": process.env.CLIENT_SECRET
-        };
-        const params = {
-            url: beforeUrl
-        }
-        
-        axios.get(URL, {params, headers })
-        .then(res=> {
-            const {data: {
-                result:{url,hash,orgUrl},message,code
-            }} = res;
-            return resolve({url,hash,orgUrl,message,code});
-        })
-        .catch(error => {
-            const {data: {errorCode , errorMessage}} = error.response;
-            return resolve({
-                code: errorCode,
-                errorMessage
-            })
-        });
-    });
+interface ShutcutUrlProps {
+  url?: string;
+  hash?: string;
+  orgUrl?: string;
+  message?: string;
+  code: string;
+  errorMessage?: string;
 }
-(async function() {
-    const url = "https://github.com/antnf3/getshutcuturl-naverapi";
-    const test = await getShutCutUrl(url);
-    console.log(test);
-})();
+function getShutCutUrl(beforeUrl: string): Promise<ShutcutUrlProps> {
+  return new Promise((resolve) => {
+    const URL = "https://openapi.naver.com/v1/util/shorturl.json";
+    const headers = {
+      "X-Naver-Client-Id": process.env.CLIENT_ID,
+      "X-Naver-Client-Secret": process.env.CLIENT_SECRET,
+    };
+    const params = {
+      url: beforeUrl,
+    };
 
+    axios
+      .get(URL, { params, headers })
+      .then((res) => {
+        const {
+          data: {
+            result: { url, hash, orgUrl },
+            message,
+            code,
+          },
+        } = res;
+        return resolve({ url, hash, orgUrl, message, code });
+      })
+      .catch((error) => {
+        const {
+          data: { errorCode, errorMessage },
+        } = error.response;
+        return resolve({
+          code: errorCode,
+          errorMessage,
+        });
+      });
+  });
+}
+(async function () {
+  const url = "https://github.com/antnf3/getshutcuturl-naverapi";
+  const test = await getShutCutUrl(url);
+  console.log(test.code);
+})();
