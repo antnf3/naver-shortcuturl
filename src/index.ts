70e5766cdaf8a05c66aaf328a7269809a7b9142e
antnf3 <antnf3@gmail.com>
2020-04-10 13:06:34 +0900

    catch구문 오류 수정

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import "dotenv/config";
 
-function getShutCutUrl() {
-    return new Promise((resolve, reject) => {
+function getShutCutUrl(beforeUrl: string) {
+    return new Promise((resolve) => {
         const URL = 'https://openapi.naver.com/v1/util/shorturl.json';
         const headers = {
             "X-Naver-Client-Id": process.env.CLIENT_ID,
             "X-Naver-Client-Secret": process.env.CLIENT_SECRET
         };
         const params = {
-            url: "https://github.com/axios/axios/issues/1744"
+            url: beforeUrl
         }
         
         axios.get(URL, {params, headers })
@@ -20,9 +20,8 @@ function getShutCutUrl() {
             return resolve({url,hash,orgUrl,message,code});
         })
         .catch(error => {
-            console.log(error.data);
             const {data: {errorCode , errorMessage}} = error.response;
-            return reject({
+            return resolve({
                 code: errorCode,
                 errorMessage
             })
@@ -30,7 +29,8 @@ function getShutCutUrl() {
     });
 }
 (async function() {
-    const test = await getShutCutUrl();
+    const url = "https://github.com/antnf3/getshutcuturl-naverapi";
+    const test = await getShutCutUrl(url);
     console.log(test);
 })();
 
